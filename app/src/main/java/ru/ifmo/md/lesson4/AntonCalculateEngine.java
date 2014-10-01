403dc83cf8418a7cc1ca8bfacb00203f2f6365f8
Anton Belyy <anton.belyy@gmail.com>
2014-10-02 02:37:00 +0400

    Implemented error reporting in calculate engine

--- app/src/main/java/ru/ifmo/md/lesson4/AntonCalculateEngine.java
+++ app/src/main/java/ru/ifmo/md/lesson4/AntonCalculateEngine.java
@@ -1,11 +1,16 @@
 package ru.ifmo.md.lesson4;
 
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 /**
  * Created by anton on 01/10/14.
  */
 public class AntonCalculateEngine implements CalculationEngine {
+    public enum Lexem {
+        OPERATOR, OPERAND, LBRACKET, RBRACKET
+    }
+
     private static boolean isOp(String op) {
         return op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/");
     }
@@ -15,7 +20,7 @@ public class AntonCalculateEngine implements CalculationEngine {
     }
 
     private static boolean isUnary(String op) {
-        return op.equals("^");
+        return op.equals("!-") || op.equals("!+");
     }
 
     private static int priority(String op) {
@@ -30,64 +35,139 @@ public class AntonCalculateEngine implements CalculationEngine {
         }
     }
 
-    private static void processOp(LinkedList<Double> st, String op) {
-        Double r = st.removeLast();
-        if (op.equals("^")) {
-            st.addLast(-r);
-        } else {
-            Double l = st.removeLast();
-            if (op.equals("+")) {
-                st.addLast(l + r);
-            } else if (op.equals("-")) {
-                st.addLast(l - r);
-            } else if (op.equals("*")) {
-                st.addLast(l * r);
-            } else if (op.equals("/")) {
-                st.addLast(l / r);
+    private static void processOp(LinkedList<Double> st, String op) throws CalculationException {
+        try {
+            Double r = st.removeLast();
+
+            if (op.equals("!-")) {
+                st.addLast(-r);
+            } else if (op.equals("!+")) {
+                st.addLast(r);
+            } else {
+                try {
+                    Double l = st.removeLast();
+
+                    if (op.equals("+")) {
+                        st.addLast(l + r);
+                    } else if (op.equals("-")) {
+                        st.addLast(l - r);
+                    } else if (op.equals("*")) {
+                        st.addLast(l * r);
+                    } else if (op.equals("/")) {
+                        st.addLast(l / r);
+                    }
+                } catch (NoSuchElementException e) {
+                    throw new CalculationException();
+                }
             }
+        } catch (NoSuchElementException e) {
+            throw new CalculationException();
         }
     }
 
     @Override
-    public double calculate(String expression) {
-        boolean mayUnaryMinus = true;
+    public double calculate(String expression) throws CalculationException {
+        Lexem lastOp = null;
         LinkedList<Double> st = new LinkedList<Double>();
         LinkedList<String> op = new LinkedList<String>();
+
         for (int i = 0; i < expression.length(); i++) {
             if (!isDelim(expression.charAt(i))) {
                 if (expression.charAt(i) == '(') {
-                    op.addLast("(");
-                    mayUnaryMinus = true;
-                } else if (expression.charAt(i) == ')') {
-                    while (!op.peekLast().equals("(")) {
-                        processOp(st, op.removeLast());
+                    if (lastOp == Lexem.RBRACKET) {
+                        // ") (" can not happen in valid expression
+                        throw new CalculationException();
+                    } else if (lastOp == Lexem.OPERAND) {
+                        // as well as "<number> ("
+                        throw new CalculationException();
                     }
+                    op.addLast("(");
+                    lastOp = Lexem.LBRACKET;
+                } else if (expression.charAt(i) == ')') {
+                    if (lastOp == null) {
+                        // ")" at the beginning of the expression
+                        throw new CalculationException();
+                    }
+                    if (lastOp == Lexem.LBRACKET) {
+                        // "()" is not valid
+                        throw new CalculationException();
+                    }
+                    try {
+                        while (!op.peekLast().equals("(")) {
+                            processOp(st, op.removeLast());
+                        }
+                    } catch (NullPointerException e) {
+                        // extra ")"
+                        throw new CalculationException();
+                    }
+
                     op.removeLast();
-                    mayUnaryMinus = false;
+                    lastOp = Lexem.RBRACKET;
                 } else if (isOp("" + expression.charAt(i))) {
                     String curOp = "" + expression.charAt(i);
-                    if (mayUnaryMinus && curOp.equals("-")) curOp = "^";
+
+                    if (lastOp != Lexem.RBRACKET && lastOp != Lexem.OPERAND) {
+                        if (curOp.equals("-")) {
+                            curOp = "!-";
+                        } else if (curOp.equals("+")) {
+                            curOp = "!+";
+                        }
+                    }
+                    if (!isUnary(curOp) && lastOp == null) {
+                        // binary operator at the beginning of the expression
+                        throw new CalculationException();
+                    }
+                    if (!isUnary(curOp) && lastOp != Lexem.OPERAND && lastOp != Lexem.RBRACKET) {
+                        // operator after another operator or left bracket
+                        throw new CalculationException();
+                    }
+
                     while (!op.isEmpty() && (!isUnary(curOp) && priority(op.peekLast()) >= priority(curOp) || isUnary(curOp) && priority(op.peekLast()) > priority(curOp))) {
                         processOp(st, op.removeLast());
                     }
+
                     op.addLast(curOp);
-                    mayUnaryMinus = true;
+                    lastOp = Lexem.OPERATOR;
                 } else {
                     StringBuilder builder = new StringBuilder();
-                    while (i < expression.length() && Character.isDigit(expression.charAt(i))) {
+
+                    while (i < expression.length() &&
+                            (Character.isDigit(expression.charAt(i))
+                             || expression.charAt(i) == '.'
+                             || Character.toLowerCase(expression.charAt(i)) == 'e'
+                             || (expression.charAt(i) == '-' && i > 0 && Character.toLowerCase(expression.charAt(i - 1)) == 'e')))
+                    {
                         builder.append(expression.charAt(i++));
                     }
                     i--;
+
                     String operand = builder.toString();
-                    Double number = Double.parseDouble(operand);
-                    st.addLast(number);
-                    mayUnaryMinus = false;
+                    if (operand.isEmpty()) {
+                        // unknown lexem
+                        throw new CalculationException();
+                    }
+                    try {
+                        Double number = Double.parseDouble(operand);
+
+                        if (lastOp == Lexem.RBRACKET || lastOp == Lexem.OPERAND) {
+                            // number after another number or rbracket
+                            throw new CalculationException();
+                        }
+
+                        st.addLast(number);
+                        lastOp = Lexem.OPERAND;
+                    } catch (NumberFormatException e) {
+                        // invalid number
+                        throw new CalculationException();
+                    }
                 }
             }
         }
+
         while (!op.isEmpty()) {
             processOp(st, op.removeLast());
         }
+
         return st.removeLast();
     }
 }
