50220a9ae36237db5072b3a44730cb554146eda7
Anton Belyy <anton.belyy@gmail.com>
2014-10-02 13:49:20 +0400

    Added hardware keyboard support

--- app/src/main/java/ru/ifmo/md/lesson4/CalculatorActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson4/CalculatorActivity.java
@@ -2,6 +2,7 @@ package ru.ifmo.md.lesson4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,35 +25,77 @@ public class CalculatorActivity extends Activity {
         this.result = (TextView) findViewById(R.id.result);
     }
 
-    public void setExpression(String expression) {
+    private void setExpression(String expression) {
         this.expression = expression;
         result.setText(expression);
+    }
+
+    private void onEquals() {
+        try {
+            double result = engine.calculate(expression);
+            setExpression(Double.toString(result));
+            errorState = Double.isInfinite(result) || Double.isNaN(result);
+        } catch (CalculationException e) {
+            setExpression("Error");
+            errorState = true;
+        }
+    }
+
+    private void onClear() {
+        setExpression("");
+    }
+
+    private void onDelete() {
+        if (!expression.isEmpty()) {
+            setExpression(expression.substring(0, expression.length() - 1));
+        }
+    }
+
+    private void validateState() {
+        if (errorState) {
+            setExpression("");
+            errorState = false;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        validateState();
+
+        char keyName = (char) event.getUnicodeChar();
+
+        if (keyName == '0' || keyName == '1' || keyName == '2' || keyName == '3' || keyName == '4' ||
+            keyName == '5' || keyName == '6' || keyName == '7' || keyName == '8' || keyName == '9' ||
+            keyName == '+' || keyName == '-' || keyName == '*' || keyName == '/' || keyName == '.' ||
+            keyName == '(' || keyName == ')') {
+                setExpression(expression + keyName);
+                return true;
+        } else if (keyCode == KeyEvent.KEYCODE_C) {
+            onClear();
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DEL) {
+            onDelete();
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_EQUALS) {
+            onEquals();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 
     public void buttonClick(View v) {
         Button button = (Button) v;
         String buttonText = button.getText().toString();
 
-        if (errorState) {
-            setExpression("");
-            errorState = false;
-        }
+        validateState();
 
         if (button.getId() == R.id.equals) {
-            try {
-                double result = engine.calculate(expression);
-                setExpression(Double.toString(result));
-                errorState = Double.isInfinite(result) || Double.isNaN(result);
-            } catch (CalculationException e) {
-                setExpression("Error");
-                errorState = true;
-            }
+            onEquals();
         } else if (button.getId() == R.id.clear) {
-            setExpression("");
+            onClear();
         } else if (button.getId() == R.id.delete) {
-            if (!expression.isEmpty()) {
-                setExpression(expression.substring(0, expression.length() - 1));
-            }
+            onDelete();
         } else {
             setExpression(expression + buttonText);
         }
