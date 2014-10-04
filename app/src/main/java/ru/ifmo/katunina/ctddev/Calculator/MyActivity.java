4b9609a8e4d2e29e94f9cf5f88814cb2c3e3d1f9
Cool Girl <evgeniya-katunina@yandex.ru>
2014-10-04 18:50:30 +0300

    Added my own parsing_lib.jar and onArithmeticClick, onEqualClick, onDeleteAllSymbolsClick, onDeleteSymbolClick.

--- app/src/main/java/ru/ifmo/katunina/ctddev/Calculator/MyActivity.java
+++ app/src/main/java/ru/ifmo/katunina/ctddev/Calculator/MyActivity.java
@@ -2,7 +2,15 @@ package ru.ifmo.katunina.ctddev.Calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
+import ru.ifmo.ctddev.katunina.Main.ExpressionParser;
+import ru.ifmo.ctddev.katunina.Main.ParsingException;
 import ru.ifmo.md.lesson4.R;
 
 /**
@@ -13,6 +21,36 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_activity_layout);
-      //  findViewsByIds();
+        findViewsByIds();
+    }
+
+    TextView tv ;
+    private void findViewsByIds() {
+        tv = ((TextView) findViewById(R.id.textView));
+    }
+
+
+    public void onArithmeticClick(View view) {
+        tv.append(((Button) view).getText());
+    }
+
+    public void onEqualClick(View view) {
+        double result = 0;
+        try {
+            result = ExpressionParser.parseDoubleExpression(tv.getText().toString()).evaluate(0.,0.,0.);
+        } catch (ParsingException e) {
+            Toast.makeText(this,R.string.invalid_expression,Toast.LENGTH_SHORT).show();
+        }
+        tv.setText(new DecimalFormat("#.############################################").format(result));
+
+    }
+
+    public void onDeleteSymbolClick(View view) {
+        String tvString = tv.getText().toString();
+        tv.setText(tvString.substring(0,tvString.length()-1));
+    }
+
+    public void onDeleteAllSymbolsClick(View view) {
+        tv.setText("");
     }
 }
