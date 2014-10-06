a6f938c8a8befbb32282dc10b8ad1d6d365358f5
PaulChristmas <pashachristmas@mail.ru>
2014-10-06 20:51:28 +0400

    Revert "Revert "Homework 4""
    This reverts commit 0c7bff798fa29ce0af6c56a88a10b7212652970f.

--- app/src/main/java/ru/ifmo/md/lesson4/CalculationEngineFactory.java
+++ app/src/main/java/ru/ifmo/md/lesson4/CalculationEngineFactory.java
@@ -6,7 +6,7 @@ public class CalculationEngineFactory {
 
     }
 
-    public static CalculationEngine defaultEngine() {
+    public static DummyCalculateEngine defaultEngine() {
         return new DummyCalculateEngine();
     }
 }
