d655c95d8d4aa5992049aba4241b512b616471a9
creed <alexchekmenev@mail.ru>
2014-10-06 23:48:50 +0400

    Well done. Good job!

--- app/src/androidTest/java/ru/ifmo/md/lesson4/tests/DummyTest.java
+++ app/src/androidTest/java/ru/ifmo/md/lesson4/tests/DummyTest.java
@@ -22,7 +22,17 @@ public class DummyTest {
     @Test
     public void testWhoppingComplex() {
         try {
+            Assert.assertEquals(5d, CalculationEngineFactory.defaultEngine().calculate("5"));
             Assert.assertEquals(10d, CalculationEngineFactory.defaultEngine().calculate("5+5"));
+            Assert.assertEquals(0d, CalculationEngineFactory.defaultEngine().calculate("5-5"));
+            Assert.assertEquals(25d, CalculationEngineFactory.defaultEngine().calculate("5*5"));
+            Assert.assertEquals(1d, CalculationEngineFactory.defaultEngine().calculate("5/5"));
+            Assert.assertEquals(0d, CalculationEngineFactory.defaultEngine().calculate("5*0"));
+            Assert.assertEquals(0d, CalculationEngineFactory.defaultEngine().calculate("0*5"));
+            Assert.assertEquals(25d, CalculationEngineFactory.defaultEngine().calculate("(5*5)"));
+            Assert.assertEquals(0d, CalculationEngineFactory.defaultEngine().calculate("(100-100)/1"));
+            Assert.assertEquals(100d, CalculationEngineFactory.defaultEngine().calculate("(100-99)*(100)"));
+            Assert.assertEquals(1d, CalculationEngineFactory.defaultEngine().calculate("(100-99+1-1.0)/1.0"));
         } catch (CalculationException e) {
             Assert.fail("Exception happened " + e);
         }
