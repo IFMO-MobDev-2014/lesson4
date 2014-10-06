e331e24b2e033fa1a18cfba14909be1699d548f3
DariaYakovleva <dariyakovleva@gmail.com>
2014-10-06 19:06:53 +0400

    all

--- app/src/androidTest/java/ru/ifmo/md/lesson4/tests/DummyTest.java
+++ app/src/androidTest/java/ru/ifmo/md/lesson4/tests/DummyTest.java
@@ -20,11 +20,61 @@ public class DummyTest {
     }
 
     @Test
-    public void testWhoppingComplex() {
+//    public void testWhoppingComplex() {
+//        try {
+//            Assert.assertEquals(10d, CalculationEngineFactory.defaultEngine().calculate("5+5"));
+//        } catch (CalculationException e) {
+//            Assert.fail("Exception happened " + e);
+//        }
+//    }
+    public void testOK(String test, double res) {
         try {
-            Assert.assertEquals(10d, CalculationEngineFactory.defaultEngine().calculate("5+5"));
+            Assert.assertEquals(res, CalculationEngineFactory.defaultEngine().calculate(test));
         } catch (CalculationException e) {
             Assert.fail("Exception happened " + e);
         }
+    }
+    public void testFAIL(String test) {
+        try {
+            Assert.assertEquals(1, CalculationEngineFactory.defaultEngine().calculate(test));
+        } catch (CalculationException e) {
+            Assert.fail("Exception happened " + e);
+        }
+    }
+    public void runTests() {
+        testOK("1+2", 1 + 2);
+        testOK("(-4)+(-5)", (-4)+(-5));
+        testOK("-5+10", -5+10);
+        testOK("45-49", 45-49);
+        testOK("-45-(-90)", -45-(-90));
+        testOK("-30-100", -30-100);
+        testOK("5*9", 5*9);
+        testOK("(-100)*4",(-100)*4);
+        testOK("(-200)*(-2)", (-200)*(-2));
+        testOK("1/2", 1/2);
+        testOK("15/5", 15/5);
+        testOK("(-100)/20", (-100)/20);
+        testOK("(((((((((((3+4+5+6+7+8+9)))))))))))", (((((((((((3+4+5+6+7+8+9))))))))))));
+        testOK("(3 + 5) * (2 + 3) / 45 + (4 * 4 + 2)", (3 + 5) * (2 + 3) / 45 + (4 * 4 + 2));
+        testOK("1 / 0.000000001", 1 / 0.000000001);
+        testOK("0.0001/0.0001", 0.0001/0.0001);
+        testOK("25-----25", 25-(-(-(-25)))); //???
+
+        testFAIL("1/0");
+        testFAIL("(94*123+323) / 0");
+        testFAIL("(-1)/0");
+        testFAIL("0/0");
+        testFAIL("1/(");
+        testFAIL("(8+(6*(4+5)");
+        testFAIL("4.00.00.9");
+        testFAIL("9.0+.0");
+        testFAIL("9.0+0.+");
+        testFAIL(".0.");
+        testFAIL("(78+8)(77-8)");
+
+
+
+
+
     }
 }
--- app/src/main/java/ru/ifmo/md/lesson4/DummyCalculateEngine.java
+++ app/src/main/java/ru/ifmo/md/lesson4/DummyCalculateEngine.java
@@ -1,8 +1,93 @@
 package ru.ifmo.md.lesson4;
 
+import android.util.Log;
+
 public class DummyCalculateEngine implements CalculationEngine {
+    private final char PLUS = '+';
+    private final char SUB = '-';
+    private final char OPEN = '(';
+    private final char MUL = '*';
+    private final char DIV = '/';
+    private String lexeme;
+    private int nextperm;
+    private final double EPS = 0.00000000001;
+
+    double number() throws CalculationException {
+        String num = "";
+        while ((lexeme.charAt(nextperm) >= '0' && lexeme.charAt(nextperm) <= '9') || (lexeme.charAt(nextperm) == '.')) {
+            num += lexeme.charAt(nextperm);
+            nextperm++;
+        }
+//        Log.d("Dummy", "num="+num);
+        if (num.isEmpty()) {
+            throw new CalculationException("Incorrect expression");
+        }
+        double res;
+        try {
+            res = Double.parseDouble(num);
+        } catch (Exception e) {
+            throw new CalculationException("Incorrect expression");
+        }
+        return res;
+    }
+
+    double multiplier() throws CalculationException {
+        double ans;
+        if (lexeme.charAt(nextperm) == OPEN) {
+            nextperm++;
+            ans = expr();
+            if (lexeme.charAt(nextperm) != ')') {
+                throw new CalculationException("Incorrect expression");
+            }
+            nextperm++;
+        } else {
+            if (lexeme.charAt(nextperm) == SUB) {
+                nextperm++;
+                ans = (-1) * multiplier();
+            } else {
+                ans = number();
+            }
+        }
+        return ans;
+    }
+
+    double summand() throws CalculationException {
+        double ans = multiplier();
+        while ((lexeme.charAt(nextperm) == MUL) || (lexeme.charAt(nextperm) == DIV)) {
+            nextperm++;
+            if (lexeme.charAt(nextperm - 1) == MUL) {
+                ans *= multiplier();
+            } else {
+                double tmp = multiplier();
+                if (tmp < EPS)
+                    throw new CalculationException("Division by zero");
+                ans /= tmp;
+            }
+        }
+        return ans;
+    }
+
+    double expr() throws CalculationException {
+        double ans = summand();
+//        Log.d("Dummy", "ans1="+ans);
+        while ((lexeme.charAt(nextperm) == PLUS) || (lexeme.charAt(nextperm) == SUB)) {
+            nextperm++;
+            if (lexeme.charAt(nextperm - 1) == PLUS) {
+                ans += summand();
+//                Log.d("Dummy", "ans2="+ans);
+            } else {
+                ans -= summand();
+            }
+        }
+        if (lexeme.charAt(nextperm) != '$') {
+            throw new CalculationException("Incorrect expression");
+        }
+        return ans;
+    }
     @Override
     public double calculate(String expression) throws CalculationException {
-        return 10;
+        nextperm = 0;
+        lexeme = expression.replaceAll("\\s+", " ").concat("$");
+        return expr();
     }
 }
