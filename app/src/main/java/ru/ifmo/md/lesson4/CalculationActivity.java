e3c8d2193dadae01e72ff13d52a27f02a5caefed
Maxim Slyusarenko <slyuslyu.m@mail.ru>
2014-10-06 17:31:19 +0400

    Small changes

--- app/src/main/java/ru/ifmo/md/lesson4/CalculationActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson4/CalculationActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 public class CalculationActivity extends Activity {
 
+    boolean numberPressedAfterResult = false;
+
     private CalculationEngine calculationEngine;
     private String expression = "";
     private TextView textView;
@@ -34,15 +36,24 @@ public class CalculationActivity extends Activity {
         Button button = (Button) v;
         String text = button.getText().toString();
         if (text.equals("1") || text.equals("2") || text.equals("3") || text.equals("4") || text.equals("5") || text.equals("6")
-        || text.equals("7") || text.equals("8") || text.equals("9") || text.equals("0") || text.equals(".") || text.equals("+")
-        || text.equals("-") || text.equals("*") || text.equals("/") || text.equals("(") || text.equals(")")) {
+        || text.equals("7") || text.equals("8") || text.equals("9") || text.equals("0") || text.equals(".")) {
+            if (numberPressedAfterResult) {
+                expression = "";
+                numberPressedAfterResult = false;
+            }
             expression += text;
             textView.setText(expression);
+        }
+        if (text.equals("+") || text.equals("-") || text.equals("*") || text.equals("/") || text.equals("(") || text.equals(")")) {
+            expression += text;
+            textView.setText(expression);
+            numberPressedAfterResult = false;
         } else if (text.equals("=")) {
             try {
                 double result = calculationEngine.calculate(expression);
                 textView.setText(Double.toString(result));
                 expression = Double.toString(result);
+                numberPressedAfterResult = true;
             } catch (CalculationException e) {
                 textView.setText(e.getMess());
             }
@@ -50,10 +61,12 @@ public class CalculationActivity extends Activity {
             if (expression.length() != 0) {
                 expression = expression.substring(0, expression.length() - 1);
                 textView.setText(expression);
+                numberPressedAfterResult = false;
             }
         } else if (text.equals("A")) {
             expression = "";
             textView.setText(expression);
+            numberPressedAfterResult = false;
         }
     }
 }
