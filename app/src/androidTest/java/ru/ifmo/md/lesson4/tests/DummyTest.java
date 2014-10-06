d43bf227cbae9cf3f0a839cc300b745b6c2a8940
= <timofeev.kirill.a@gmail.com>
2014-10-06 23:44:02 +0400

    first commit

--- app/src/androidTest/java/ru/ifmo/md/lesson4/tests/DummyTest.java
+++ app/src/androidTest/java/ru/ifmo/md/lesson4/tests/DummyTest.java
@@ -8,23 +8,82 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import ru.ifmo.md.lesson4.CalculationEngine;
 import ru.ifmo.md.lesson4.CalculationEngineFactory;
 import ru.ifmo.md.lesson4.CalculationException;
 
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class DummyTest {
+    private CalculationEngine calc;
+
     @Before
     public void setup() {
+        calc = CalculationEngineFactory.defaultEngine();
         //do whatever is necessary before every test
+    }
+    // tests from year2013 with some additions
+    @Test
+    public void testCorrectness() {
+        expectEqual("(--3)", 3.0);
+        expectEqual("-(+(-(+7)))", 7.0);
+        expectEqual("1/-5", -0.2);
+        expectEqual("72.0/532*729*177/699-267.0*471/481", -236.466070483464);
+        expectEqual("-(+(-18.0/36*(10.0+13.0/36/16))+(-21.0/7*24+21.0*41+6.0))", -789.988715277778);
+        expectEqual("-25/(21-7*3)", Double.NEGATIVE_INFINITY);
+        expectEqual("7-.1", 6.9);
+        expectEqual("-3-(-3)", 0.0);
+        expectEqual("-(-(-8))", -8.0);
+        expectEqual("-3", -3.0);
+        expectEqual("+3", 3.0);
+        expectEqual("9.", 9.0);
+        expectEqual("1/0", Double.POSITIVE_INFINITY);
+        expectEqual("0/0", Double.NaN);
+        expectEqual("99999999999999999999999999999999999999999999999999", 99999999999999999999999999999999999999999999999999.0);
     }
 
     @Test
-    public void testWhoppingComplex() {
+    public void testFailures() {
+        expectFail("()");
+        expectFail("((");
+        expectFail("(");
+        expectFail(")");
+        expectFail("(+)");
+        expectFail("(-3)(-3)");
+        expectFail("-(3)-");
+        expectFail("-3(+2)");
+        expectFail("*7*(3+3)");
+        expectFail(")+(3)");
+        expectFail("(7+(7+3)");
+        expectFail("9-");
+        expectFail("(6)(8)");
+        expectFail("(4)9");
+        expectFail("(4).");
+        expectFail(".(4)");
+        expectFail("5(4)");
+        expectFail("1-1(4)");
+        expectFail("3)");
+        expectFail("(3");
+        expectFail("7.0.0");
+        expectFail("(");
+        expectFail("(-.-)");
+        expectFail(".");
+    }
+
+    public void expectFail(String s) {
         try {
-            Assert.assertEquals(10d, CalculationEngineFactory.defaultEngine().calculate("5+5"));
-        } catch (CalculationException e) {
-            Assert.fail("Exception happened " + e);
+            calc.calculate(s);
+            Assert.fail("Unable to fail on " + s);
+        } catch (CalculationException ex) {
+            // ok
+        }
+    }
+
+    public void expectEqual(String s, double d) {
+        try {
+            Assert.assertEquals(calc.calculate(s), d, 1e-10);
+        } catch (CalculationException ex) {
+            Assert.fail("Unexpected exception: " + ex.getMessage());
         }
     }
 }
