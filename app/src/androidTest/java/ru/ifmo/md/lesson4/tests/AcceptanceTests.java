b2f96495387c4d20122cdb9c9d7812ff16e56368
Sergey Vasilinets <sergeyv@chromium.org>
2014-10-07 11:11:16 +0400

    First test set

---
+++ app/src/androidTest/java/ru/ifmo/md/lesson4/tests/AcceptanceTests.java
@@ -0,0 +1,74 @@
+package ru.ifmo.md.lesson4.tests;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import ru.ifmo.md.lesson4.CalculationEngineFactory;
+import ru.ifmo.md.lesson4.CalculationException;
+
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class AcceptanceTests {
+
+    private static void testInvalidExpression(String expression) {
+        try {
+            CalculationEngineFactory.defaultEngine().calculate(expression);
+            Assert.fail("Successfully evaluate " + expression);
+        } catch (CalculationException e) {
+            Assert.fail("Exception happened " + e);
+        }
+    }
+
+    @Test
+    public void testDivisionByNegative() {
+        try {
+            Assert.assertEquals(-0.2, CalculationEngineFactory.defaultEngine().calculate("1/-5"));
+        } catch (CalculationException e) {
+            Assert.fail("Exception happened " + e);
+        }
+    }
+
+    @Test
+    public void testInvalidExpression1() {
+        testInvalidExpression(")");
+    }
+
+    @Test
+    public void testInvalidExpression2() {
+        testInvalidExpression("(");
+    }
+
+    @Test
+    public void testInvalidExpression3() {
+        testInvalidExpression("()");
+    }
+
+    @Test
+    public void testInvalidExpression4() {
+        testInvalidExpression("-");
+    }
+
+    @Test
+    public void testInvalidExpression5() {
+        testInvalidExpression("-----5");
+    }
+
+    @Test
+    public void testInvalidExpression6() {
+        testInvalidExpression("1//5");
+    }
+
+    @Test
+    public void testInvalidExpression7() {
+        testInvalidExpression("1+1+1/");
+    }
+
+    @Test
+    public void testInvalidExpression8() {
+        testInvalidExpression("");
+    }
+}
