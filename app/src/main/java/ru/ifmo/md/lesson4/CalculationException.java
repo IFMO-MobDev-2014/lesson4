8a938b7d9bd17d8b94a5ab04145d01f691416df8
AntonLuxy <borzenko_a@bk.ru>
2015-01-22 19:52:49 +0300

    Update CalculationException.java

--- app/src/main/java/ru/ifmo/md/lesson4/CalculationException.java
+++ app/src/main/java/ru/ifmo/md/lesson4/CalculationException.java
@@ -1,10 +1,7 @@
 package ru.ifmo.md.lesson4;
 
 public class CalculationException extends Exception {
-<<<<<<< HEAD
     public CalculationException(String message) {
         super(message);
     }
-=======
->>>>>>> aa1abad81687c79e37ccbd0aec782e36f808139a
 }
