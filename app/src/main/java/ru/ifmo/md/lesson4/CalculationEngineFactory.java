3775f443d34bee6a380916150f945be08c788728
AntonLuxy <borzenko_a@bk.ru>
2015-01-22 19:52:18 +0300

    Update CalculationEngineFactory.java

--- app/src/main/java/ru/ifmo/md/lesson4/CalculationEngineFactory.java
+++ app/src/main/java/ru/ifmo/md/lesson4/CalculationEngineFactory.java
@@ -7,10 +7,6 @@ public class CalculationEngineFactory {
     }
 
     public static CalculationEngine defaultEngine() {
-<<<<<<< HEAD
         return new MyCalculatorEngine();
-=======
-        return new DummyCalculateEngine();
->>>>>>> aa1abad81687c79e37ccbd0aec782e36f808139a
     }
 }
